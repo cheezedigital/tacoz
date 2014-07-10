fb7e65dd23ab4e53ef8a675f12304e666f6e8f07
Justin Veirs <justinveirs@gmail.com>
2014-07-10 11:11:20 -0600

    added controller and pages to show our food

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get '/about' => 'site#about'
   get '/contact' => 'site#contact'
   post '/contact-submit' => 'site#contact_submit', as: :submit_contact
-
+  resources :menu_items, only: [:index, :show], path: 'our-food'
+  #gives us by default. says we have
+  #some controller named menu_itmes, and we want all the resource controller actions, however
+  #we only want two of them, which is index, and show.
 end
