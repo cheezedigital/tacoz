e27b78406d921e2b1f0f799672e04c5dadf3c434
Justin Veirs <justinveirs@gmail.com>
2014-07-18 11:28:08 -0600

    adds logout to html

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,19 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def twitter #whichever provider we're implementing, def that.
-    binding.pry
+    @user = User.find_or_create_by_omniauth(auth_hash)
+
+    if @user.persisted?
+      sign_in @user
+      redirect_to root_path, notice: 'User now signed in with Twitter'
+    else
+      redirect_to root_path, alert: 'Could not sign in'
+    end
   end
 
+  private
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:twitter]
+
+  def self.find_or_create_by_omniauth(auth_hash)
+    instance = find_or_initialize_by(provider: auth_hash['provider'], uid: auth_hash[:uid])
+    instance.save(validate: false)
+    instance
+  end
 end
